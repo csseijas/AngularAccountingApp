3cc6bcbfab9434f1694348e74adf0b5d446994a9
Formacion689 <Formacion689@stkproj.com>
2018-05-09 10:22:48 +0200

    Added Material-Menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule, MatButtonModule, MatIconModule, MatCardModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountabilityMenuComponent } from './countability-menu/countability-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountabilityMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
